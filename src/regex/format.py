b9f3d30b98837e80e5609838715ea75e1b299ee4
vdella <vitor.origamer@gmail.com>
2022-05-26 18:45:40 -0300

    added method to check if a symbol is surrounded by parenthesis

--- src/regex/format.py
+++ src/regex/format.py
@@ -16,14 +16,21 @@ def __add_missing_concatenations(regex):
     by the concatenation operator will have it added."""
     stripped = list(regex)
     for i in range(len(stripped)):
-        if stripped[i] not in operators - {'.'} and stripped[i] not in parenthesis and not __is_surrounded(regex, i):
+        if stripped[i] not in operators - {'.'} and stripped[i] not in parenthesis \
+                and not __non_terminal_surrounded(regex, i) and not __left_parenthesis_surrounded(regex, i):
             stripped[i] = '.' + stripped[i]
     return ''.join(stripped)  # Turns a list of strings into a single string.
 
 
-def __is_surrounded(regex: str, place) -> bool:
+def __non_terminal_surrounded(regex: str, place) -> bool:
     """Checks if a character is surrounded by non-terminals by any of its sides."""
     return regex[place - 1] in non_terminals and regex[place + 1] in non_terminals
+
+
+def __left_parenthesis_surrounded(regex: str, place) -> bool:
+    """As the missing concatenations will be added by the left of a symbol,
+    we have to avoid adding concatenations at its left if there is a '('."""
+    return regex[place - 1] == '(' and regex[place] in __terminals_from(regex)
 
 
 def __terminals_from(regex) -> set:
@@ -41,4 +48,6 @@ def __trim_blank_spaces(regex: str) -> str:
 
 
 if __name__ == '__main__':
-    print(eat('(a | b)*abb'))
+    print(__terminals_from('(ab)*abb'))
+    print(__left_parenthesis_surrounded('(ab)*abb', 1))
+    print(eat('(a|b)*abb'))
