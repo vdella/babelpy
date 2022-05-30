c0a8bfb794dc5b9c8fece547148be708d9397534
vdella <vitor.origamer@gmail.com>
2022-05-30 04:24:18 -0300

    re-added label to state

--- src/structures/automata/state.py
+++ src/structures/automata/state.py
@@ -1,19 +1,20 @@
 class State:
 
-    def __init__(self, identifier: int):
+    def __init__(self, identifier: int, label: str = None):
         self.id = identifier
+        self.label = label
 
     def __str__(self):
-        return 'q{}'.format(self.id)
+        return self.label if self.label is not None else 'q{}'.format(self.id)
 
     def __hash__(self):
         return hash(self.id)
 
     def __eq__(self, other):
-        if isinstance(other, State):
-            return self.id == other.id
+        return self.id == other.id
 
 
 if __name__ == '__main__':
     s = set()
     s.add(State(0))
+    print(s.pop())
