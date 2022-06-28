17561a2cfa46ca6ffb30099a84f59e7d090aaeee
vdella <vitor.origamer@gmail.com>
2022-06-28 01:36:22 -0300

    added functions to read and write grammars into and from files

---
+++ src/grammars/persistency/writer.py
@@ -0,0 +1,14 @@
+from src.grammars.cfg import ContextFreeGrammar
+from src import resource_dir
+from src.grammars.persistency.reader import read_grammar_from
+
+
+def write(cfg: ContextFreeGrammar, filename='generated_grammar.txt'):
+    with open(resource_dir / filename, 'w') as f:
+        f.write(str(cfg))
+
+
+if __name__ == '__main__':
+    grammar = read_grammar_from('a_even_b_even.txt')
+    print(grammar)
+    write(grammar)
