1f99977a552d04592782f794cd5f7a395a746d29
vdella <vitor.origamer@gmail.com>
2022-07-11 20:35:33 -0300

    changed grammar read() and write() functions
    	* Grammars are now organized in a similar way
    	  as the FAs previously implemented. Each grammar
    	  file has to be divided in 4 different sections:
    
    		* #start
    		* #non-terminals
    		* #terminals
    		* #productions
    
    	  This was necessary to cover more complex grammars, as those
    	  will have terminals and non-terminals with more than
    	  1 char to describe them, such as id, cmd, var etc.
    	* Following the function alterations, the resources were
    	  also changed to match the new read() and write().

--- src/grammars/persistency/reader.py
+++ src/grammars/persistency/reader.py
@@ -1,47 +1,29 @@
 from src import resource_dir
 from src.grammars.structures.cfg import ContextFreeGrammar
-from ordered_set import OrderedSet
-from collections import OrderedDict
 
 
 def read_grammar_from(filepath) -> ContextFreeGrammar:
     with open(resource_dir / filepath, 'r') as f:
         lines = f.readlines()
 
-    trimmed = __trim(lines)
+    trimmed = [line.rstrip() for line in lines]
 
-    productions = OrderedDict()
-    for production in trimmed:
-        non_terminal = production[0]  # Expected to always be non-terminal.
-        productions[non_terminal] = set(production[1:])  # Without '->', only productions are left in the line.
+    start = trimmed[trimmed.index('#start') + 1]
+    non_terminals = {symbol for symbol in trimmed[trimmed.index('#non-terminals') + 1: trimmed.index('#terminals')]}
+    terminals = {symbol for symbol in trimmed[trimmed.index('#terminals') + 1: trimmed.index('#productions')]}
 
-    terminals = set()
-    non_terminals = OrderedSet([non_terminal for non_terminal in productions.keys()])
-    for _, arrival in productions.items():
-        unified = ''.join(arrival)  # In order to avoid O(n^3), turns the list into a single string...
+    productions = dict()
+    for production in trimmed[trimmed.index('#productions') + 1:]:
+        line_without_arrow = production.replace(' ', '').split('->')
+        non_terminal = line_without_arrow[0]  # Expected to always be non-terminal.
+        digested_line = ''.join(line_without_arrow[1:]).split('|')
+        productions[non_terminal] = set(digested_line)
 
-        for symbol in unified:  # And searches its insides, symbol by symbol.
-            if symbol.islower():
-                terminals |= {symbol}
-            elif symbol.isupper():
-                non_terminals |= {symbol}
-
-    return ContextFreeGrammar(non_terminals, terminals, productions)
-
-
-def __trim(lines) -> list:
-    """Removes arrows and slashes from :param lines."""
-    trimmed = list()
-
-    for line in lines:
-        correted = line.replace('->', '').replace('|', '').split()
-        trimmed.append(correted)
-
-    return trimmed
+    return ContextFreeGrammar(non_terminals, terminals, productions, start)
 
 
 if __name__ == '__main__':
-    grammar = read_grammar_from('reduced_grammar2.txt')
+    grammar = read_grammar_from('a_even_b_even.txt')
     print(grammar)
 
     for key, value in grammar.first().items():
