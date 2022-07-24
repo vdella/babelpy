e6456d1bbc0b5911167e6d5a96ee0efe71b51a67
arthurwillrich <arthur.willrich@gmail.com>
2022-07-24 20:13:43 -0300

    Pilha e Tabela funcionais

--- src/grammars/structures/analysis_table.py
+++ src/grammars/structures/analysis_table.py
@@ -1,3 +1,5 @@
+import copy
+
 from src.automata.structures.state import State
 from prettytable import PrettyTable
 #from src.grammars.persistency.reader import read_grammar_from
@@ -14,6 +16,7 @@ class AnalysisTableContrcutor:
         self.non_terminals = ''
         self.split_productions = ''
         self.table = ''
+        self.split_productions = dict()
 
     def read(self):
 
@@ -57,7 +60,7 @@ class AnalysisTableContrcutor:
 
     def generate_table(self):
         non_terminals = list(self.non_terminals)
-        terminals = list(self.terminals) + ["$"]
+        terminals = (list(self.terminals) + ['$'])
         table = {nt: {t: str() for t in terminals} for nt in non_terminals}
 
         i = 1
@@ -69,6 +72,7 @@ class AnalysisTableContrcutor:
                 split_productions[i] = ((self.productions[p])[c-1])
                 aux_dict[i] = p
                 i = i+1
+        self.split_productions = split_productions
 
         for non_terminal in self.non_terminals:
             firsts = self.firsts[non_terminal]
@@ -76,6 +80,8 @@ class AnalysisTableContrcutor:
                 if (split_productions[derivation])[0] in self.terminals:
                     if table[non_terminal][(split_productions[derivation])[0]] == '' and aux_dict[derivation] == non_terminal:
                         if split_productions[derivation] == '&':
+                            for follow in self.follows[non_terminal]:
+                                table[non_terminal][follow] = derivation
                             table[non_terminal][('$')[0]] = derivation
                         else:
                             table[non_terminal][(split_productions[derivation])[0]] = derivation
@@ -83,21 +89,86 @@ class AnalysisTableContrcutor:
                     for first in firsts:
                         if table[non_terminal][first] == '' and aux_dict[derivation] == non_terminal:
                             table[non_terminal][first] = derivation
+
         self.table = table
 
+    def run_analysis(self, sentence):
+        stacktrace = []
+        stack = ['$']
+        stack.append(self.start_state)
+        entry = sentence + '$'
+        accepted = False
+
+        variables = self.non_terminals
 
 
+        while entry != '' and stack != '':
+            history = {"stack": stack, "entry": entry}
+            stacktrace.append(copy.deepcopy(history))
+            symbol = stack.pop()
+            print(stack)
+
+            if symbol in variables:
+                if symbol in self.table and entry[0] in self.table[symbol]:
+                    derivation = self.table[symbol][entry[0]]
+                    if derivation == '':
+                        accepted = False
+                        break
+
+                    derivation = list(self.split_productions[derivation])
+                    i = 0
+                    for var in derivation:
+                        if var == "'":
+                            derivation[i - 1] = derivation[i - 1] + "'"
+                            del derivation[i]
+                        i = i + 1
+                    derivation.reverse()
+                    if derivation[0] == "&":
+                        continue
+                    stack += derivation
+                    print(stack)
+                else:
+                    accepted = False
+                    break
+            elif symbol == entry[0]:
+                if symbol == "$":
+                    accepted = True
+                    break
+                else:
+                    entry = entry[1:]
+            else:
+                accepted = False
+                print('2')
+                break
+        print(accepted)
+        return stacktrace, accepted
 
 
 analysisTable = AnalysisTableContrcutor()
-
 analysisTable.read()
 
+
 table = PrettyTable()
+
 header = ['NonTerminal']
 for terminal in analysisTable.terminals:
     header.append(terminal)
-table.field_names = header
-analysisTable.generate_table()
+header.append('$')
 
-print(analysisTable.table)
+table.field_names = header
+
+analysisTable.generate_table()
+analysisTable.run_analysis('ivi^i')
+
+#for column in analysisTable.table:
+   # lista = []
+    #for line in analysisTable.table[column]:
+       # print("teste: " ,(analysisTable.table[column])[line])
+
+    #print((analysisTable.table[column])['i'])
+
+
+
+
+
+
