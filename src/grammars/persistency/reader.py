678fb27cdda8cb88231a9a97e0baa0e64689fce9
vdella <vitor.origamer@gmail.com>
2022-07-24 22:27:29 -0300

    fixed conflicts

--- src/grammars/persistency/reader.py
+++ src/grammars/persistency/reader.py
@@ -9,20 +9,36 @@ def read_grammar_from(filepath) -> ContextFreeGrammar:
 
     trimmed = [line.rstrip() for line in lines]
 
-    start = trimmed[trimmed.index('#start') + 1]
-    non_terminals = {symbol for symbol in trimmed[trimmed.index('#non-terminals') + 1: trimmed.index('#terminals')]}
-    terminals = {symbol for symbol in trimmed[trimmed.index('#terminals') + 1: trimmed.index('#productions')]}
+    start = __start_symbol_from(trimmed)
+    non_terminals = __non_terminals_from(trimmed)
+    terminals = __terminals_from(trimmed)
+    productions = __digest_productions_from(trimmed)
 
+    return ContextFreeGrammar(non_terminals, terminals, productions, start)
+
+
+def __start_symbol_from(lines):
+    return lines[lines.index('#start') + 1]
+
+
+def __non_terminals_from(lines):
+    return {symbol for symbol in lines[lines.index('#non-terminals') + 1: lines.index('#terminals')]}
+
+
+def __terminals_from(lines):
+    return {symbol for symbol in lines[lines.index('#terminals') + 1: lines.index('#productions')]}
+
+
+def __digest_productions_from(lines):
     productions = dict()
-    for production in trimmed[trimmed.index('#productions') + 1:]:
+
+    for production in lines[lines.index('#productions') + 1:]:
         line_without_arrow = production.replace(' ', '').split('->')
         non_terminal = line_without_arrow[0]  # Expected to always be non-terminal.
         digested_line = ''.join(line_without_arrow[1:]).split('|')
-        productions[non_terminal] = set(digested_line)
+        productions[non_terminal] = list(digested_line)
 
-
-
-    return ContextFreeGrammar(non_terminals, terminals, productions, start)
+    return productions
 
 
 def __show_first_from(cfg):
@@ -32,7 +48,20 @@ def __show_first_from(cfg):
     print()
 
 
+def __show_follow_from(cfg):
+    for key, value in cfg.follow().items():
+        if key.isupper():
+            print(key, value)
+    print()
+
+
 if __name__ == '__main__':
-    grammar3 = read_grammar_from('reduced_grammar1.txt')
-    __show_first_from(grammar3)
-    # __show_first_from(grammar3)
+    grammar1 = read_grammar_from('reduced_grammar1.txt')
+    # print(__organize_patterns(grammar1.symbols, grammar1.productions))
+
+    __show_first_from(grammar1)
+    # __show_follow_from(grammar1)
+    #
+    # grammar2 = read_grammar_from('reduced_grammar2.txt')
+    # __show_first_from(grammar2)
+    # __show_follow_from(grammar2)
